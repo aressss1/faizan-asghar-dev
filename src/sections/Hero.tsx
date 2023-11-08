fb020e005dfb0f3db12055b19d0b6c07eafd998b
aressss1 <codewithfaizi@gmail.com>
2023-11-08 12:13:09 +0500

    Hero T4-

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -51,7 +51,7 @@ function Hero() {
           delay: 1.35,
         }}
       >
-        I&apos;m a Web Developer with a passion for crafting outstanding digital experiences. My current focus is on partnering with agencies and startups to fuel their growth and success. I thrive on turning ideas into web realities and <span className="link">helping businesses thrive in the digital landscape</span>
+        I&apos;m a Web Developer with a passion for crafting outstanding digital experiences. My current focus is on partnering with <span className="link">agencies</span> and <span className="link" >startups</span>to fuel their growth and success. I thrive on turning ideas into web realities and helping businesses thrive in the digital landscape
       </motion.p>
       <motion.div
         className="hero-button"
