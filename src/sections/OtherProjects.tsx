c5962dfec86771fdc939aa1a3e573cce331ef3fc
aressss1 <codewithfaizi@gmail.com>
2023-11-15 17:22:49 +0500

    op-

--- src/sections/OtherProjects.tsx
+++ src/sections/OtherProjects.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 function OtherProjects() {
-  return <div  >
-    OtherProjects</div>;
+  return <div className="op" >
+    <h2 className="op-heading" >Other Projects</h2>
+
+    <div className="op-card" >
+      <div className="op-card-inner" >
+      <div className="op-card-top" >
+        folderIcon
+        Goto Icons
+      </div>
+      <h2 className="op-card-heading" >
+        Lorem, ipsum dolor.
+      </h2>
+      <p className="op-card-description" >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda inventore pariatur ipsum? Fuga deserunt eius, sint blanditiis consectetur, rem adipisci fugiat, voluptate quibusdam ut culpa vel voluptatum rerum! Eos, tenetur illo.
+      </p>
+      <div className="op-card-footer" >
+        <p>React</p>
+      </div>
+    </div>
+  </div>
+
+    </div >;
 
 }
 
