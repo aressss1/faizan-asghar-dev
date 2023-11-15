5fd5a3cffc2c5870e1e3a736c8557a6d30bc3a44
aressss1 <codewithfaizi@gmail.com>
2023-11-15 20:14:25 +0500

    op-

--- src/sections/OtherProjects.tsx
+++ src/sections/OtherProjects.tsx
@@ -75,7 +75,7 @@ function OtherProjects() {
             hidden: { opacity: 0, y: 0 },
           }}
           whileHover={{ 
-            scale: 1.05,
+            y: -10
           }}
           key={project.title}
           onClick={() => router.push(`${project.link}`)}
