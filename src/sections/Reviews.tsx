1950c4583cdb207ef4b128f777c71290f2f0fcfa
aressss1 <codewithfaizi@gmail.com>
2023-11-16 11:24:36 +0500

    rev-

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -1,9 +1,40 @@
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
+
 const Reviews = () => {
-    return ( 
-        <div>
-            Review
+    return (
+        <div id="reviews" className="review">
+    
+            <div className="rev-inner">
+                <div className="rev-img-div">
+                    <Image  
+                     className="rev-img"
+                     src="https://via.placeholder.com/68x68"
+                     alt="client-1" 
+                     />
+                </div>
+
+                <div className="rev-star-div">
+                    <FaStar className="rev-star-icon" />
+                    <FaStar className="rev-star-icon" />
+                    <FaStar className="rev-star-icon" />
+                    <FaStar className="rev-star-icon" />
+                    <FaStar className="rev-star-icon" />
+                </div>
+
+                <div className="rev-desc">Everyone&apos;s on the same page. Many of our people are not very organized naturally, so Pharps is a godsend!</div>
+
+                <div className="rev-footer-div">
+
+                    <div className="rev-footer-name">Ms. Tony Rutherford</div>
+
+                    <div className="rev-footer-from">
+                        From Fiverr
+                    </div>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Reviews;
