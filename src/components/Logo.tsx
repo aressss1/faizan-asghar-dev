3ac9aebe33386df5314f8b300d76b6d6b6c6b9a1
aressss1 <codewithfaizi@gmail.com>
2023-11-16 18:28:51 +0500

    l-

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,10 +9,7 @@ function Logo() {
           {/* Replace this path data with the SVG path data for the letter "F" */}
           <path
             fill="currentColor"
-            d="M75,5
-            l-60,0 0,90
-            M15,45
-            l50,0"
+            d="M16 12C16 10.8954 16.8954 10 18 10L30 10C31.1046 10 32 10.8954 32 12C32 13.1046 31.1046 14 30 14L20 14L20 22L30 22C31.1046 22 32 22.8954 32 24C32 25.1046 31.1046 26 30 26L20 26L20 36C20 37.1046 19.1046 38 18 38C16.8954 38 16 37.1046 16 36L16 12Z"
           />
         </g>
         {/* Keep the rest of the SVG elements unchanged */}
@@ -35,3 +32,4 @@ function Logo() {
 
 export default Logo;
 
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" viewBox="0 0 48 48" id="letter-f"><path fill="#333" fill-rule="evenodd"  clip-rule="evenodd"></path></svg>
