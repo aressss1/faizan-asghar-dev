9ede8dbdd2ea26491c6c607ab474c01113ff73ae
aressss1 <codewithfaizi@gmail.com>
2023-11-17 12:04:42 +0500

    fav-

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,9 +26,8 @@ function Index() {
     <div className="app">
       <Head>
         <title>Faizan Asghar</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon"  href="/favicon.ico" sizes="any" />
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
 
       </Head>
       {showContent && (
