ab40702406d3c9aecd2ade47e333f52d73f6a0c9
aressss1 <codewithfaizi@gmail.com>
2023-11-17 16:34:26 +0500

    l-

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,29 +26,31 @@ function Index() {
     <div className="app">
       <Head>
         <title>Faizan Asghar</title>
-        <link rel="icon"  href="/favicon.ico" sizes="any" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+              <link rel="manifest" href="/site.webmanifest"/>
 
-      </Head>
-      {showContent && (
-        <>
-          <Navbar />
-          <SocialIcons />
-          <Email />
-          <main>
-            <Hero />
-            <About />
-            <Projects />
-            <OtherProjects />
-            <Reviews />
-            <Contact />
-          </main>
-          <Footer />
-        </>
-      )}
-      <Loader isLoading={isLoading} setIsLoading={handleLoaderLoaded} />
-    </div>
-  );
+              </Head>
+              {showContent && (
+                <>
+                  <Navbar />
+                  <SocialIcons />
+                  <Email />
+                  <main>
+                    <Hero />
+                    <About />
+                    <Projects />
+                    <OtherProjects />
+                    <Reviews />
+                    <Contact />
+                  </main>
+                  <Footer />
+                </>
+              )}
+              <Loader isLoading={isLoading} setIsLoading={handleLoaderLoaded} />
+            </div>
+            );
 }
 
-export default Index;
+            export default Index;
