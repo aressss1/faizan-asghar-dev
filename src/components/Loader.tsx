690fbd9b64685804206e978ecdbf59a740ddc1f8
aressss1 <codewithfaizi@gmail.com>
2023-11-17 09:34:55 +0500

    l-

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Logo from "./Logo";
 import { AnimatePresence, motion } from "framer-motion";
-import Logo from "./Logo"; // Import your Logo component with the "F" SVG
 
 function Loader({ isLoading, setIsLoading }: any) {
   useEffect(() => {
@@ -8,7 +8,6 @@ function Loader({ isLoading, setIsLoading }: any) {
       setIsLoading();
     }, 1900);
   }, [setIsLoading]);
-
   return (
     <AnimatePresence>
       {isLoading && (
@@ -28,7 +27,22 @@ function Loader({ isLoading, setIsLoading }: any) {
           >
             <title>Logo</title>
             <g>
-              <Logo /> {/* Use the Logo component here */}
+              <g id="F" transform="translate(25, 25)">
+                <motion.path
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    duration: 1,
+                    ease: "easeInOut",
+                    delay: 1,
+                  }}
+                  exit={{
+                    scale: 2,
+                  }}
+                  fill="currentColor"
+                  d="M16 12C16 10.8954 16.8954 10 18 10L30 10C31.1046 10 32 10.8954 32 12C32 13.1046 31.1046 14 30 14L20 14L20 22L30 22C31.1046 22 32 22.8954 32 24C32 25.1046 31.1046 26 30 26L20 26L20 36C20 37.1046 19.1046 38 18 38C16.8954 38 16 37.1046 16 36L16 12Z"
+                />
+              </g>
               <motion.path
                 initial={{ pathLength: 0 }}
                 animate={{ pathLength: 1 }}
@@ -44,11 +58,11 @@ function Loader({ isLoading, setIsLoading }: any) {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
+          L 11, 27
+          L 11, 72
+          L 50, 95
+          L 89, 73
+          L 89, 28 z"
               />
             </g>
           </motion.svg>
@@ -59,4 +73,3 @@ function Loader({ isLoading, setIsLoading }: any) {
 }
 
 export default Loader;
-
