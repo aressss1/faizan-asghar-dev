d624f88c84ec11c6883045ba046f6f81984ea991
aressss1 <codewithfaizi@gmail.com>
2023-11-17 09:11:31 +0500

    l-

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,8 +5,8 @@ function Logo() {
     <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
       <title>Logo</title>
       <g>
-        <g id="F" transform="translate(35.000000, 35.000000)">
-          {/* Replace this path data with the SVG path data for the letter "F" */}
+        <g id="F" transform="translate(25, 25)">
+          {/* Adjusted translate values to center the F */}
           <path
             fill="currentColor"
             d="M16 12C16 10.8954 16.8954 10 18 10L30 10C31.1046 10 32 10.8954 32 12C32 13.1046 31.1046 14 30 14L20 14L20 22L30 22C31.1046 22 32 22.8954 32 24C32 25.1046 31.1046 26 30 26L20 26L20 36C20 37.1046 19.1046 38 18 38C16.8954 38 16 37.1046 16 36L16 12Z"
@@ -32,4 +32,4 @@ function Logo() {
 
 export default Logo;
 
-<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" viewBox="0 0 48 48" id="letter-f"><path fill="#333" fill-rule="evenodd"  clip-rule="evenodd"></path></svg>
+
