33d2e3cd79716c75a17c331661e4b9f3977dd38f
aressss1 <codewithfaizi@gmail.com>
2023-11-17 17:36:52 +0500

    rev-

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -50,26 +50,27 @@ const Reviews = () => {
                 <h2 className="main-title-h2" >Client&apos;s Review</h2>
             </div>
 
-            <div className="rev-card" >
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                showDots={false}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                infinite={true}
+                // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                // autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                // deviceType={this.props.deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+            >
+                <div className="rev-card" >
 
-                <Carousel
-                    swipeable={false}
-                    draggable={false}
-                    showDots={false}
-                    responsive={responsive}
-                    ssr={true} // means to render carousel on server-side.
-                    infinite={true}
-                    // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-                    // autoPlaySpeed={1000}
-                    keyBoardControl={true}
-                    customTransition="all .5"
-                    transitionDuration={500}
-                    containerClass="carousel-container"
-                    removeArrowOnDeviceType={["tablet", "mobile"]}
-                    // deviceType={this.props.deviceType}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
-                >
+
                     {revInfo.map((rev) => (
 
                         <div key={rev.name} className="review">
@@ -112,9 +113,9 @@ const Reviews = () => {
 
 
                     ))}
-                </Carousel>
 
-            </div>
+                </div>
+            </Carousel>
 
         </div >
     );
