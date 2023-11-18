96e35e8e3a205dcb22bed5c25e3f4869dbaff46f
aressss1 <codewithfaizi@gmail.com>
2023-11-18 17:44:26 +0500

    f-

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,135 +1,3 @@
-// import Button from "@/components/Button";
-// import Logo from "@/components/Logo";
-// import Link from "next/link";
-// import { GiHamburgerMenu } from "react-icons/gi";
-// import { CgClose } from "react-icons/cg";
-// import React, { useEffect, useState } from "react";
-// import { motion } from "framer-motion";
-// function Navbar() {
-//   const [navbarVisible, setNavbarVisible] = useState(false);
-//   const [responsiveNavVisible, setResponsiveNavVisible] = useState(false);
-//   const sectionLinks = [
-//     { name: "About", link: "/#about" },
-//     { name: "Work", link: "/#work" },
-//     { name: "Review", link: "/#reviews" },
-//     { name: "Contact", link: "/#contact",},
-//   ];
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", () => {
-//       window.pageYOffset > 100
-//         ? setNavbarVisible(true)
-//         : setNavbarVisible(false);
-//     });
-//   }, []);
-
-//   useEffect(() => {
-//     const links = document.querySelectorAll(".nav-items-list-item-link");
-//     links.forEach((link) => {
-//       link.addEventListener("click", () => setResponsiveNavVisible(false));
-//     });
-//     const nav = document.querySelector(".nav-items");
-//     nav?.addEventListener("click", (e) => {
-//       e.stopPropagation();
-//     });
-//     const html = document.querySelector("html");
-//     html?.addEventListener("click", (e) => {
-//       setResponsiveNavVisible(false);
-//     });
-//   }, []);
-
-//   useEffect(() => {
-//     const main = document.querySelector("main");
-//     if (responsiveNavVisible) {
-//       main?.classList.add("blur");
-//     } else {
-//       main?.classList.remove("blur");
-//     }
-//   }, [responsiveNavVisible]);
-
-//   return (
-//     <nav>
-//       <div className={`wrapper ${navbarVisible ? "blur-nav" : ""}`}>
-//         <motion.div
-//           className="brand"
-//           initial={{ opacity: 0 }}
-//           animate={{ opacity: 1 }}
-//           transition={{
-//             duration: 0.3,
-//             ease: "easeInOut",
-//           }}
-//         >
-//           <Link href="/">
-//             <Logo />
-//           </Link>
-//         </motion.div>
-//         <motion.div
-//           className="nav-responsive-toggle"
-//           initial={{ opacity: 0, y: 5 }}
-//           animate={{ opacity: 1, y: 0 }}
-//           transition={{
-//             duration: 0.3,
-//             ease: "easeInOut",
-//           }}
-//         >
-//           {responsiveNavVisible ? (
-//             <CgClose
-//               onClick={(e) => {
-//                 e.stopPropagation();
-//                 setResponsiveNavVisible(false);
-//               }}
-//             />
-//           ) : (
-//             <GiHamburgerMenu
-//               onClick={(e) => {
-//                 e.stopPropagation();
-//                 setResponsiveNavVisible(true);
-//               }}
-//             />
-//           )}
-//         </motion.div>
-//         <div
-//           className={`${responsiveNavVisible && "nav-responsive"} nav-items`}
-//         >
-//           <ul className="nav-items-list">
-//             {sectionLinks.map(({ name, link }, index) => (
-//               <motion.li
-//                 key={name}
-//                 className="nav-items-list-item"
-//                 initial={{ opacity: 0, y: -25 }}
-//                 animate={{ opacity: 1, y: 0 }}
-//                 transition={{
-//                   duration: 0.3,
-//                   ease: "easeInOut",
-//                   delay: 0.3 + index * 0.1,
-//                 }}
-//               >
-//                 <Link href={link} className="nav-items-list-item-link">
-//                   {name}
-//                 </Link>
-//               </motion.li>
-//             ))}
-//           </ul>
-//           <motion.div
-//             className="nav-items-button"
-//             initial={{ opacity: 0, y: -25 }}
-//             animate={{ opacity: 1, y: 0 }}
-//             transition={{
-//               duration: 0.3,
-//               ease: "easeInOut",
-//               delay: 0.6,
-//             }}
-//           >
-//             <Button text="My GitHub" classname="btn" link="https://github.com/aressss1" />
-//           </motion.div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 import Button from "@/components/Button";
 import Logo from "@/components/Logo";
 import Link from "next/link";
@@ -137,31 +5,15 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { CgClose } from "react-icons/cg";
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-
 function Navbar() {
   const [navbarVisible, setNavbarVisible] = useState(false);
   const [responsiveNavVisible, setResponsiveNavVisible] = useState(false);
   const sectionLinks = [
-    { name: "About", link: "#about" },
-    { name: "Work", link: "#work" },
-    { name: "Review", link: "#reviews" },
-    { name: "Contact", link: "#contact" },
+    { name: "About", link: "/#about" },
+    { name: "Work", link: "/#work" },
+    { name: "Review", link: "/#reviews" },
+    { name: "Contact", link: "/#contact",},
   ];
-
-  const handleSectionClick = (link: string) => {
-    const section = document.querySelector(link);
-    console.log(section)
-    
-    if (section instanceof HTMLElement ) {
-      const navbarHeight = document.querySelector('nav')?.offsetHeight || 0;
-      const sectionPosition = section.offsetTop - navbarHeight;
-      window.scrollTo({
-        top: sectionPosition,
-        behavior: 'smooth',
-      });
-      setResponsiveNavVisible(false); // Close responsive nav on click
-    }
-  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -207,7 +59,9 @@ function Navbar() {
             ease: "easeInOut",
           }}
         >
-          <Logo />
+          <Link href="/">
+            <Logo />
+          </Link>
         </motion.div>
         <motion.div
           className="nav-responsive-toggle"
@@ -250,16 +104,9 @@ function Navbar() {
                   delay: 0.3 + index * 0.1,
                 }}
               >
-                <a
-                  href={link}
-                  className="nav-items-list-item-link"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleSectionClick(link);
-                  }}
-                >
+                <Link href={link} className="nav-items-list-item-link">
                   {name}
-                </a>
+                </Link>
               </motion.li>
             ))}
           </ul>
@@ -282,3 +129,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
